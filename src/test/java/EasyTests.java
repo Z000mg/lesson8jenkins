93e031ed91ab0ee2a4708bc30033a12946d10c90
z000mg <zooomg2015@yandex.ru>
2020-12-05 14:24:44 +0300

    done

---
+++ src/test/java/EasyTests.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class EasyTests {
+    int a = 5;
+    int b = 8;
+
+    @Test
+    @Tag("bad_test")
+    void falseTest () {
+        assertNotEquals(a, b, "a not equals b");
+    }
+
+    @Test
+    @Tag("ok_test")
+    void trueTest () {
+        assertEquals(a * b, 40, "a * b equals 40");
+    }
+
+    @Test
+    @Tag("don't_test_it")
+    void dontDoTest () {
+        assertEquals(a * b, "сорок", "a * b not equals 'сорок'");
+    }
+}
+
